8f48b9b1ce7f74df89e429176ff6ce66b41dc504
Hendrick Garcia da Rocha <hendrick.rocha2133@gmail.com>
2020-06-22 02:37:52 -0300

    concerteiBug

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Query } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +7,60 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+n1:number;
+n2:number;
+resultadoFinal: number;
+  constructor() { }
 
+  fnSoma() {
+    console.log("Função de soma foi acionada!");
+    this.n1 = parseFloat( this.n1.toString());
+    this.n2 = parseFloat( this.n2.toString());
+    console.log("n1", this.n1);
+    console.log("n2", this.n2);
+
+    this.resultadoFinal = (this.n1 + this.n2);
+    console.log("Resultado", this.resultadoFinal);
+  }
+
+  fnSub() {
+    console.log("Função de subtração foi acionada!");
+    this.n1 = parseFloat( this.n1.toString());
+    this.n2 = parseFloat( this.n2.toString());
+    console.log("n1", this.n1);
+    console.log("n2", this.n2);
+
+    this.resultadoFinal = (this.n1 - this.n2);
+    console.log("Resultado", this.resultadoFinal);
+  }
+
+  fnMult() {
+    console.log("Função de multiplicação foi acionada!");
+    this.n1 = parseFloat( this.n1.toString());
+    this.n2 = parseFloat( this.n2.toString());
+    console.log("n1", this.n1);
+    console.log("n2", this.n2);
+
+    this.resultadoFinal = (this.n1 * this.n2);
+    console.log("Resultado", this.resultadoFinal);
+  }
+
+  fnDiv() {
+    console.log("Função de divisão foi acionada!");
+    this.n1 = parseFloat( this.n1.toString());
+    this.n2 = parseFloat( this.n2.toString());
+    console.log("n1", this.n1);
+    console.log("n2", this.n2);
+
+    this.resultadoFinal = (this.n1 / this.n2);
+    console.log("Resultado", this.resultadoFinal);
+  }
+
+  fnIgual() {
+    console.log("Função de igual foi acionada!");
+  }
+  
+  fnClear() {
+    console.log("Função de limpar foi acionada!");
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { SSL_OP_NO_TLSv1_2 } from 'constants';
+import { parse } from 'path';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,72 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  visorCalculadora: string = "";
+  operador: string;
+  valorAcumulado: number = 0;
+  visorOperador: string = "";
+  
+  
+
+  constructor() { }
+
+  clicou(valor: any) {
+    //console.log(valor);
+    this.visorCalculadora += valor;
+  }
+
+  op(op: any) {
+    this.visorOperador = op;
+    
+  }
+
+  clear(click: any) {
+    this.visorCalculadora = click;
+  }
+
+  clearAll(click: any) {
+    this.visorCalculadora = click;
+    this.visorOperador = click;
+  }
+
+  armazenar() {
+
+    this.valorAcumulado = parseFloat(this.visorCalculadora);
+  }
+
+  acao() {
+
+    var results 
+    var aux
+    aux = parseFloat(this.visorCalculadora);
+    switch (this.visorOperador) {
+      case "X":
+        results = this.valorAcumulado * aux;
+        this.visorCalculadora = results
+        break;
+
+      case "-":
+        results = this.valorAcumulado - aux;
+        this.visorCalculadora = results
+        break;
+
+      case "+":
+        results = this.valorAcumulado + aux;
+        this.visorCalculadora = results
+        break;
+
+      case "/":
+        results = this.valorAcumulado / aux;
+        this.visorCalculadora = results
+        break;
+    }
+
+    console.log("VALORACUMLADO:", this.valorAcumulado);
+    console.log("AUX:", aux);
+    console.log("results:", results);
+    //console.log('Operador: ', this.operador);
+    // this.valorAcumulado = parseFloat(this.visorCalculadora);
+    // this.visorOperador = "" + this.results;
+  }
 
 }
